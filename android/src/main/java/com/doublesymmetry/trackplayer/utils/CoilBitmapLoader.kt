f753fb5c790d712cb0fca21d4f42e80d0059764f
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-10-07 11:34:18 -0700

    feat: crop square albumart

--- android/src/main/java/com/doublesymmetry/trackplayer/utils/CoilBitmapLoader.kt
+++ android/src/main/java/com/doublesymmetry/trackplayer/utils/CoilBitmapLoader.kt
@@ -5,12 +5,14 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
+import android.os.Build
 import androidx.media3.common.util.BitmapLoader
 import androidx.media3.common.util.Util.isBitmapFactorySupportedMimeType
 import androidx.media3.common.util.UnstableApi
 import coil.ImageLoader
 import coil.request.ImageRequest
 import com.google.common.util.concurrent.ListenableFuture
+import jp.wasabeef.transformers.coil.CropSquareTransformation
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.guava.future
 import java.io.IOException
@@ -19,7 +21,9 @@ import javax.inject.Inject
 // https://github.com/androidx/media/issues/121
 
 @UnstableApi
-class CoilBitmapLoader @Inject constructor(private val context: Context) : BitmapLoader {
+class CoilBitmapLoader @Inject constructor(
+    private val context: Context,
+    private val cropSquare: Boolean = false) : BitmapLoader {
 
     private val scope = MainScope()
     private val imageLoader = ImageLoader(context)
@@ -36,14 +40,17 @@ class CoilBitmapLoader @Inject constructor(private val context: Context) : Bitma
     }
 
     override fun loadBitmap(uri: Uri): ListenableFuture<Bitmap> = scope.future {
-        val request = ImageRequest.Builder(context)
+        var imgrequest = ImageRequest.Builder(context)
             .data(uri)
             .allowHardware(false)
-            .build()
+        // HACK: header implementation should be done via parsed data from uri
 
-        val response = imageLoader.execute(request)
+        if (Build.MANUFACTURER == "samsung" || cropSquare) {
+            imgrequest = imgrequest.transformations(CropSquareTransformation())
+        }
+
+        val response = imageLoader.execute(imgrequest.build())
         val bitmap = (response.drawable as? BitmapDrawable)?.bitmap
-        bitmap
-            ?: throw IOException("Unable to load bitmap: $uri")
+        bitmap ?: throw IOException("Unable to load bitmap: $uri")
     }
 }
